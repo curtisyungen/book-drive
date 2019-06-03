921813232aaa9becdf6613af892728241a487688
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-02 17:57:08 -0700

    Add files via upload

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Container from "../components/Container/container";
 import Book from "../components/Book/book";
-// import API from "../utils/API";
+import API from "../utils/API";
 import "./Home.css";
 
 class Home extends Component {
@@ -19,45 +19,20 @@ class Home extends Component {
         this.getBooks();
 
         this.setState({
-            books: [
-                {
-                    title: "Getting Everything You Can Out of All You've Got",
-                    author: "Jay Abraham",
-                    price: "0.00",
-                    imageURL: "https://images-na.ssl-images-amazon.com/images/I/81SOM0kcgdL.jpg",
-                    avail: true,
-                    tags: ["red", "green"],
-                },
-
-                {
-                    title: "Speaking Up Without Freaking Out",
-                    author: "Matt Abrahams",
-                    price: "0.00",
-                    imageURL: "https://images-na.ssl-images-amazon.com/images/I/51UhYXbDwkL._SX355_BO1,204,203,200_.jpg",
-                    avail: true,
-                    tags: ["red", "green"],
-                },
-
-                {
-                    title: "The Wall Street MBA",
-                    author: "Reuben Advani",
-                    price: "0.00",
-                    imageURL: "https://s3-ap-southeast-1.amazonaws.com/mph-images/9780071788311_b.jpg",
-                    avail: true,
-                    tags: ["red", "green"],
-                }
-            ],
+            books: [],
         });
     }
 
     getBooks = () => {
-        // API.getAllBooks()
-        //     .then((res) => {
-        //         console.log(res);
-        //     })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     });
+        API.getAllBooks()
+            .then((res) => {
+                this.setState({
+                    books: res.data,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 
     render() {
