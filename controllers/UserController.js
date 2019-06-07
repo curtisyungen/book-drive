3c062da38f961eec16512b614b6e4f0c02e8bf8e
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-06 21:04:29 -0700

    Add files via upload

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,6 +3,21 @@ const bcrypt = require("bcrypt");
 
 class UserController {
 
+    loginUser(req, res) {
+        db.Users.findOne({
+            where: {
+                email: req.body.email,
+                password: req.body.password,
+            }
+        })
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
     createNewUser(req, res) {
 
         // bcrypt
--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 const UserController = require("../../controllers/UserController");
 const controller = new UserController();
 
+router.get("/loginUser", (req, res) => {
+    controller.loginUser(req, res);
+});
+
 router.post("/createNewUser", (req, res) => {
     controller.createNewUser(req, res);
 });
