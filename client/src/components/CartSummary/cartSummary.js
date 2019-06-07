697153d5f692c778f42c1856ce3be94b1b848af0
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-06 20:20:25 -0700

    Add files via upload

--- client/src/components/CartSummary/cartSummary.js
+++ client/src/components/CartSummary/cartSummary.js
@@ -31,21 +31,21 @@ class CartSummary extends Component {
             <div
                 className="cartSummary"
             >
-                <p>Subtotal: {this.state.subtotal}</p>
+                <div>Subtotal:
+                    <span className="subtotal">&nbsp;{`$${this.state.subtotal}`}</span>
+                </div>
 
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                    <input type="hidden" name="cmd" value="_s-xclick" />
-                    <input
-                        type="hidden"
-                        name="encrypted"
-                        value="-----BEGIN PKCS7-----MIIHLwYJKoZIhvcNAQcEoIIHIDCCBxwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAk8YSgmm9Hgq+kDlhLzCxEphfDgtskMsibNGdp9q5TQwHptt5Ks21tsZhcY5waGhZfa7wWlrFcG3L1dfwRW7ju3m4AzvAXetd5/GKMyKKjFAShkiKD2Mcglsi0/hMW9P5BroCdHAqs4GEKQNKVXTy1lxsc5NwIOeyU6q9PdwxAGzELMAkGBSsOAwIaBQAwgawGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIYJxvcevrajGAgYhq3tT5pu4MzJo3viW2+vuwfIBy/ccC4ks3AQ0FP148zUWn/a6Wkt4nErthAeRhNHsCS7HD0EebU4ds8hHDk51pkVi1mGNvHqnccnTt72eRZEIlivsh2An+aea3XDUmpyBlTeOew02EyOHaHaBZ9LbdqzoVf0uIQ8cYUGy+E6SXzeSHA5qzdpj9oIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTkwNjA3MDI1MzI4WjAjBgkqhkiG9w0BCQQxFgQUUuIeYnwOs99mRoJ04oy1NPfDllkwDQYJKoZIhvcNAQEBBQAEgYCe4+5nS04gD2eSpr4vyew6AFK4QVB+4rkf1dyVJByfKBPALcWsv+Ndl5/7vtJu0SlX8pRgGppfYF9WGV2jC9UCo/CtMR95uvI7enc8RYAq14dC1uu9IRQ5rGAykssC9CIMFcfZC7C/mRrWZxoD/OGKFtsQMh+3NUxKYfSnnMNQAA==-----END PKCS7-----" />
-                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-                </form>
+                <br/>
+
+                <button
+                    className="checkoutBtn"
+                >
+                    <a className="proceedToCheckout" href="/checkout">Proceed to checkout</a>
+                </button>
 
             </div>
-                    )
-                }
-            }
-            
+        )
+    }
+}
+
 export default CartSummary;
