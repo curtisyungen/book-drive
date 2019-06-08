32853621f1d2c4fc90a4f1198a04cd9560bc352f
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-07 21:42:05 -0700

    Add files via upload

--- models/books.js
+++ models/books.js
@@ -21,7 +21,7 @@ module.exports = function(sequelize, DataTypes) {
         },
 
         avail: {
-            type: DataTypes.BOOLEAN,
+            type: DataTypes.STRING,
             allowNull: false,
         },
 
