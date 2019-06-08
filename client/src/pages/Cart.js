751e7e51f143e33669e3ec301253c848b369134b
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-07 22:32:17 -0700

    Add files via upload

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -16,6 +16,7 @@ class Cart extends Component {
 
     componentDidMount = () => {
         this.getCartFromLocalStorage();
+        this.getSubtotal();
     }
 
     getCartFromLocalStorage = () => {
