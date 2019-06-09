71fd4ec1de734b9f195a1f8eb46d0768fba9c6a3
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-09 14:09:55 -0700

    Add files via upload

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -35,7 +35,7 @@ class Navbar extends Component {
     }
 
     getUserFromLocalStorage = () => {
-        if (localStorage.getItem("user") !== null) {
+        if (localStorage.getItem("isLoggedIn") === true && localStorage.getItem("user") !== null) {
             let user = JSON.parse(localStorage.getItem("user"));
             let firstName = user.name.split(" ", 1);
 
