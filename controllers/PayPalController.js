f20ce7dad28ce8755afefb834fd9c70ee85ce8f1
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-12 07:56:36 -0700

    Add files via upload

--- controllers/PayPalController.js
+++ controllers/PayPalController.js
@@ -49,6 +49,44 @@ class PayPalController {
                 });
         });
     }
+
+    successfulPayment(req, res) {
+        const payerId = req.query.PayerId;
+        const paymentId = req.query.paymentId;
+
+        const execute_payment_json = {
+            "payer_id": payerId,
+            "transactions": [{
+                "amount": {
+                    "currency": "USD",
+                    "total": "5.00",
+                }
+            }]
+        };
+
+        paypal.payment.execute(paymentId, execute_payment_json, function(error, payment) {
+            if (error) {
+                console.log(error.response);
+                throw error;
+            }
+            else {
+
+                console.log(JSON.stringify(payment));
+
+                db.Books.findOne({})
+                    .then(() => {
+                        res.json(payment);
+                    });
+            }
+        });
+    }
+
+    cancelPayment (req, res) {
+        db.Books.findOne({})
+            .then(() => {
+                res.json("Cancelled.");
+            });
+    }
 }
 
 module.exports = PayPalController;
