1433aa7549451b52f20a07da33ea79ee5ba44f6d
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-12 21:37:53 -0700

    Add files via upload

--- controllers/PayPalController.js
+++ controllers/PayPalController.js
@@ -22,7 +22,7 @@ class PayPalController {
             },
             "transactions": [{
                 "item_list": {
-                    "items": [req.body]
+                    "items": [req.body.items]
                 },
                 "amount": {
                     "currency": "USD",
