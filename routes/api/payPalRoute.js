aa540aa2ade0ea364d12ca8a4ee05dc883444184
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-12 19:14:21 -0700

    Add files via upload

--- routes/api/payPalRoute.js
+++ routes/api/payPalRoute.js
@@ -2,6 +2,15 @@ const router = require("express").Router();
 const PayPalController = require("../../controllers/PayPalController");
 const controller = new PayPalController();
 
+
+router.get("/successfulPayment", (req, res) => {
+    controller.successfulPayment(req, res);
+});
+
+router.get("/cancelPayment", (req, res) => {
+    controller.cancelPayment(req, res);
+});
+
 router.post("/payUsingPayPal", (req, res) => {
     controller.payUsingPayPal(req, res);
 });
