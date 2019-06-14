d229598cad0f0be8fea946665a9669e173638c63
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-14 14:09:08 -0700

    Add files via upload

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -73,7 +73,7 @@ export default {
     },
 
     payUsingPayPal: function(total) {
-        return axios.post("/api/payPal/payUsingPayPal/" + total);
+        return axios.post("/api/payPal/payUsingPayPal/" + total, { 'headers': { 'Content-Type': 'application/json' } });
     },
 
     successfulPayment: function() {
