a975700aea843849a6b80c332600303a0b8d7fb1
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-16 15:36:06 -0700

    Add files via upload

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,18 +4,36 @@ const bcrypt = require("bcrypt");
 class UserController {
 
     loginUser(req, res) {
+
         db.Users.findAll({
             where: {
                 email: req.body.email,
-                password: req.body.password,
             }
         })
         .then((user) => {
-            res.json(user);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+            bcrypt.compare(req.body.password, user.password, function(err, response) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                if (response) {
+                    res.json(user);
+                }
+            });
+        });        
+
+        // db.Users.findAll({
+        //     where: {
+        //         email: req.body.email,
+        //         password: req.body.password,
+        //     }
+        // })
+        // .then((user) => {
+        //     res.json(user);
+        // })
+        // .catch((err) => {
+        //     console.log(err);
+        // });
     }
 
     createNewUser(req, res) {
@@ -24,8 +42,6 @@ class UserController {
             if (err) {
                 return console.log(err);
             }
-            
-            console.log("Salt", salt);
 
             bcrypt.hash(req.body.password, salt, function(err, hash) {
                 db.Users.create({
@@ -41,28 +57,6 @@ class UserController {
                 });
             });
         });
-
-        // bcrypt
-        //     .hash(req.body.password, 10)
-        //     .then(hash => {
-
-        //         console.log("Hash", hash);
-
-                // db.Users.create({
-                //     name: req.body.name,
-                //     email: req.body.email,
-                //     password: req.body.password,
-                // })
-                // .then((user) => {
-                //     res.json(user);
-                // })
-                // .catch((err) => {
-                //     console.log(err);
-                // });
-            // })
-            // .catch((err) => {
-            //     console.log(err);
-            // });
     }
 
     findExistingUser(req, res) {
