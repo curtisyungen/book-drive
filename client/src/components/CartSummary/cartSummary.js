5a6f053fcb9722bf4522702cb2159e2161a41e95
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-17 12:20:04 -0700

    Add files via upload

--- client/src/components/CartSummary/cartSummary.js
+++ client/src/components/CartSummary/cartSummary.js
@@ -48,24 +48,16 @@ class CartSummary extends Component {
         //     items.push(item);
         // }
 
-        items = [
-            {
+        items = [{
                 name: "Test",
                 sku: "001",
-                price: "1.00",
+                price: "5.00",
                 currency: "USD",
                 quantity: 1
-            },
-            {
-                name: "Test",
-                sku: "002",
-                price: "1.00",
-                currency: "USD",
-                quantity: 1
-            },
-        ]
+            }];
+        
 
-        API.payUsingPayPal(items, "2.00")
+        API.payUsingPayPal(items, "5.00")
             .then((res) => {
                 console.log(res);
 
