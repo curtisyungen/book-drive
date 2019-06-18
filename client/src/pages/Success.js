c2548b24bf4573638cb9c742fa63ccaaf70c4b22
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-17 17:32:25 -0700

    Add files via upload

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import Container from "../components/Container/container";
+import Container from "../components/Container/container";
 import API from "../utils/API";
 import "./Success.css";
 
@@ -9,7 +9,7 @@ class Success extends Component {
         super(props);
 
         this.state = {
-
+            shippingAddress: "",
         }
     }
 
@@ -17,24 +17,33 @@ class Success extends Component {
 
         // Get payment info from URL
         let parameters = window.location.href.split("?")[1].split("&");
-
         let paymentId = parameters[0].split("=")[1];
-        let token = parameters[1].split("=")[1];
         let payerId = parameters[2].split("=")[1];
 
-        console.log("paymentId", paymentId);
-        console.log("token", token);
-        console.log("payerId", payerId);
-
+        // Process payment
         API.successfulPayment(paymentId, payerId)
             .then((res) => {
                 console.log("Success", res);
+
+                // If payment approved, get shipping address
+                if (res.data.state === "approved") {
+                    this.setState({
+                        shippingAddress: res.data.transactions[0].item_list.shipping_address,
+    
+                    }, () => {
+                        console.log(this.state);
+                    });
+                }
             });
     }
 
     render() {
         return (
-            <></>
+            <Container>
+                <div>
+                    {this.state.shippingAddress}
+                </div>
+            </Container>
         )
     }
 }
