c00375c5f3471844c8110576046aca17ca169749
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-17 17:56:40 -0700

    Add files via upload

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -35,7 +35,7 @@ class Home extends Component {
         }
 
         this.setState({
-            books: this.props.books,
+            // books: this.props.books,
             message: message,
             userSearch: this.props.userSearch,
             sortOption: "",
@@ -47,15 +47,10 @@ class Home extends Component {
     componentDidUpdate = (prevProps) => {
         if (prevProps !== this.props) {
             this.setState({
-                books: this.props.books,
+                // books: this.props.books,
                 userSearch: this.props.userSearch,
             });
         }
-    }
-
-    scrollToTop = () => {
-        window.scrollTo(0, 0);
-        // this.hideScrollToTopBtn();
     }
 
     getActiveFilter = (filter) => {
@@ -174,15 +169,6 @@ class Home extends Component {
                                 sendToCart={this.props.sendToCart}
                             />
                             ))}
-                            <button
-                                className="btn btn-outline-dark btn-sm backToTopBtn"
-                                onClick={(event) => {
-                                    event.preventDefault();
-                                    this.scrollToTop();
-                                }}
-                            >
-                                Back to Top
-                            </button>
                         </span>
                     ) : (
                             <div>{this.state.message}</div>
