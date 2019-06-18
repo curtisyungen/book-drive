a1966b77998aa344d52d1229d7967e33832c1bf6
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-18 16:09:16 -0700

    Add files via upload

---
+++ controllers/OrderController.js
@@ -0,0 +1,38 @@
+const db = require("../models/index.js");
+
+class OrderController {
+
+    getUserBookOrders(req, res) {
+        db.Orders.findAll({
+            where: {
+                email: req.params.email,
+            }
+        })
+        .then((orders) => {
+            res.json(orders);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+
+    createBookOrder(req, res) {
+        db.Orders.create({
+            buyerName: req.body.name,
+            buyerEmail: req.body.email,
+            date: req.body.date,
+            totalPrice: req.body.totalPrice,
+            items: req.body.items,
+            itemQty: req.body.itemQty,
+            shippingAddress: req.body.shippingAddress,
+        })
+        .then((order) => {
+            res.json(order);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+}
+
+module.exports = OrderController;
