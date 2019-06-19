0ec27aeb37fb6a107b442adecd753fbcb9903d72
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-18 17:54:47 -0700

    Add files via upload

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -6,17 +6,25 @@ import API from "../utils/API";
 import "./Success.css";
 
 class Success extends Component {
-    
+
     constructor(props) {
         super(props);
 
         this.state = {
+            user: null,
+            cart: null,
             shippingAddress: "",
             order: null,
         }
     }
 
     componentDidMount = () => {
+
+        // Store user and cart in state
+        this.setState({
+            user: this.props.user,
+            cart: this.props.cart,
+        });
 
         // Get payment info from URL
         let parameters = window.location.href.split("?")[1].split("&");
@@ -27,62 +35,81 @@ class Success extends Component {
         API.successfulPayment(paymentId, payerId)
             .then((res) => {
 
-                // If payment approved, get shipping address
+                // If payment approved, get shipping and order info
                 if (res.data.state === "approved") {
+
+                    // Get total from cart
+                    let cart = this.props.cart;
+                    let total = 0;
+                    for (var book in cart) {
+                        total += cart[book].price;
+                    }
+
+                    // Assemble order info
+                    let order = {
+                        name: this.state.user.name,
+                        email: this.state.user.email,
+                        date: new Date(),
+                        totalPrice: total,
+                        items: JSON.stringify(this.props.cart),
+                        itemQty: this.props.cart.length,
+                        shippingAddress: JSON.stringify(this.state.shippingAddress),
+                    }
+
+                    // Store shipping and order info in state
                     this.setState({
                         shippingAddress: res.data.transactions[0].item_list.shipping_address,
-                        order: res.data.transactions[0].item_list.items,
-                        user: this.props.user,
-                        cart: this.props.cart,
+                        order: order,
                     }, () => {
-                        this.saveBookOrder();
+
+                        // Store order in database
+                        this.saveBookOrder(order);
+
+                        // Update book purchase status in database
+                        this.purchaseBook(cart);
                     });
                 }
             });
     }
 
-    saveBookOrder = () => {
-
-        let cart = this.props.cart;
-        let total = 0;
-
-        for (var book in cart) {
-            total += cart[book].price;
-        }
-
-        let order = {
-            name: this.state.user.name,
-            email: this.state.user.email,
-            date: new Date(),
-            totalPrice: total,
-            items: JSON.stringify(this.props.cart),
-            itemQty: this.props.cart.length,
-            shippingAddress: JSON.stringify(this.state.shippingAddress),
-        }
-
-        console.log("Order", order);
-
+    saveBookOrder = (order) => {
         API.createBookOrder(order)
             .then((res) => {
                 console.log("Result", res);
-                alert("Order saved!");
             });
+    }
+
+    purchaseBook = (cart) => {
+
+        let book;
+        for (var b in cart) {
+            book = {
+                title: cart[b].title,
+                authorFirst: cart[b].authorFirst,
+                authorLast: cart[b].authorLast,
+            }
+
+            API.purchaseBook(book, this.state.user.email)
+                .then((res) => {
+                    console.log(res);
+                });
+        }
     }
 
     render() {
         return (
             <div className="orderConfirmation">
-                
+
                 <div>
                     <p className="thankYou">Thank you, your order has been placed.</p>
                     <p className="confirmation">Please check your email for order confirmation.</p>
                 </div>
 
-                <OrderDetail 
+                <OrderDetail
                     order={this.state.order}
                 />
 
-                <ShippingInfo 
+                <ShippingInfo
                     shippingAddress={this.state.shippingAddress}
                 />
 
