2d1a213a31bdbfc59f07ea9e9bb0965a92ab489c
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-18 18:33:29 -0700

    Add files via upload

---
+++ client/src/pages/Orders.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import "./Orders.css";
+
+class Orders extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            orders: null,
+        }
+    }
+
+    componentDidMount = () => {
+        this.setState({
+            user: this.props.user,
+        }, () => {
+            this.getOrders();
+        });
+    }
+
+    getOrders = () => {
+        API.getUserBookOrders(this.state.user.email)
+            .then((res) => {
+                console.log(res);
+            });
+    }
+
+    render() {
+        return (
+            <></>
+        )
+    }
+}
+
+export default Orders;
