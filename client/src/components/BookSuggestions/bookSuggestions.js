df11ed65d033988583f834c6ea02ef59ec518388
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-19 06:31:37 -0700

    Add files via upload

---
+++ client/src/components/BookSuggestions/bookSuggestions.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Book from "../Book/book";
+import API from "../../utils/API";
+import "./bookSuggestions.css";
+
+class BookSuggestions extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            suggestions: null,
+        }
+    }
+
+    componentDidMount = () => {
+        API.getBookSuggestions()
+            .then((res) => {
+                this.setState({
+                    suggestions: res.data,
+                });
+            });
+    }
+
+    render() {
+        return (
+            <div className="bookSuggestions">
+                {this.state.suggestions ? (
+                    this.state.suggestions.map(book => (
+                        <Book 
+                            title={book.title}
+                            authorFirst={book.authorFirst}
+                            authorLast={book.authorLast}
+                            price={book.price}
+                            avail={book.avail}
+                            tags={book.tags}
+                            imageURL={book.imageURL}
+                        />
+                    ))
+                ) : (
+                    <></>
+                )}
+            </div>
+        )
+    }
+}
+
+export default BookSuggestions;
