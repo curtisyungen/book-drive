86a73fba9f5adcddca90e73718f90ad8d09be7b8
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-20 11:16:17 -0700

    Add files via upload

---
+++ client/src/components/Footer/footer.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./footer.css";
+
+// import logo from "../../images/congo2.png";
+
+library.add(faSearch, faShoppingCart);
+
+class Footer extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="footer">
+                Footer
+            </div>
+        )
+    }
+}
+
+export default Footer;
