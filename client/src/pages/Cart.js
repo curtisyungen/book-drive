9942b93555b6c468051925e99063638aa84addb3
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-20 20:00:50 -0700

    Add files via upload

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -86,7 +86,7 @@ class Cart extends Component {
 
                     <BookSuggestions
                         displayClass="cart"
-                        positionClass={`cart-empty-${this.state.cart && this.state.cart.length === 0 ? (true):(false)}`}
+                        positionClass={`cart-empty-${this.state.cart && this.state.cart.length > 0 ? ("true"):("false")}`}
                     />
 
                     <table
