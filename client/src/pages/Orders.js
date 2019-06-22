026365b1994e1d52259ca0cf6fb8650030f8c740
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-22 16:07:27 -0700

    Add files via upload

--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -28,40 +28,42 @@ class Orders extends Component {
     getOrders = () => {
         if (this.props.user || this.state.user) {
             API.getUserBookOrders(this.props.user.email || this.state.user.email)
-            .then((res) => {
-                this.setState({
-                    orders: res.data,
+                .then((res) => {
+                    this.setState({
+                        orders: res.data,
+                    });
                 });
-            });
         }
     }
 
     render() {
         return (
-            <Container>
-                <h4 className="yourOrdersHeader">Your Orders</h4>
+            <span>
+                <Container>
+                    <h4 className="yourOrdersHeader">Your Orders</h4>
 
-                {this.state.orders ? (
-                    this.state.orders.map(order => (
-                        <PastOrder
-                            date={order.date}
-                            items={order.items}
-                            price={order.totalPrice}
-                        />
-                    ))
-                ) : (
-                    <p>No orders found.</p>
-                )}
+                    {this.state.orders ? (
+                        this.state.orders.map(order => (
+                            <PastOrder
+                                date={order.date}
+                                items={order.items}
+                                price={order.totalPrice}
+                            />
+                        ))
+                    ) : (
+                            <p>No orders found.</p>
+                        )}
 
-                <BookSuggestions 
-                    displayClass="order"
-                    sendToCart={this.props.sendToCart}
-                />
+                    <BookSuggestions
+                        displayClass="order"
+                        sendToCart={this.props.sendToCart}
+                    />
+
+                </Container>
 
                 <Footer />
 
-            </Container>
-
+            </span>
         )
     }
 }
