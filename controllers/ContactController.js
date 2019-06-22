55c68effc342a365258a93717031f4b3cf8806a4
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-21 18:42:01 -0700

    Add files via upload

---
+++ controllers/ContactController.js
@@ -0,0 +1,38 @@
+const db = require("../models/index.js");
+const nodemailer = require("nodemailer");
+
+class ContactController {
+
+    sendContactMessage(req, res) {
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "congoserver@gmail.com",
+                pass: process.env.GMAIL_PASSWORD,
+            }
+        });
+
+        let mailOptions = {
+            from: "congoserver@gmail.com",
+            to: "congobooksales@gmail.com",
+            subject: "Message from Congo user",
+            text: req.params.message,
+        };
+
+        transporter.sendMail(mailOptions, function (err, info) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                // Used only to fulfill axios promise
+                db.Books.findAll({})
+                    .then(() => {
+                        res.json("Sent");
+                    });
+            }
+        });
+    }
+
+}
+
+module.exports = ContactController;
