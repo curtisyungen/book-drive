54e0fcc490f5a9ae146c809abbdf2c27a6441a2e
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-21 22:06:57 -0700

    Add files via upload

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Container from "../components/Container/container";
+import Footer from "../components/Footer/footer";
 import allBooks from "../images/allBooks.jpg";
 import fiftyBooks from "../images/fiftyBooks.JPG";
 import robertGreene from "../images/robertGreene.JPG";
@@ -7,55 +7,60 @@ import cardoneBooks from "../images/cardoneBooks.PNG";
 import daringGreatly from "../images/daringGreatly.jpg";
 import dietCults from "../images/dietCults.JPG";
 import earlyShelf from "../images/earlyShelf.JPG";
+import firstFullShelf from "../images/firstFullShelf.JPG";
+import midBooks from "../images/midBooks.jpg";
+import handstand from "../images/handstand.jpg";
 import "./Gallery.css";
 
 class Gallery extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-
-        }
-    }
-
-    componentDidMount = () => {
-
-    }
-
     render() {
         return (
-            <Container>
-                
-                <div>
-                    <img className="earlyShelf galleryImg" src={earlyShelf} alt="Early Shelf" />
-                    <p className="caption">Early Shelf</p>
-                </div>
-                <div>
-                    <img className="allBooks galleryImg" src={allBooks} alt="All Books" />
-                    <p className="caption">The collection</p>
-                </div>
-                
-                <div>
-                    <img className="robertGreene galleryImg" src={robertGreene} alt="Robert Greene" />
-                    <p className="caption">Robert Greene's books</p>
-                </div>
-                <div>
-                    <img className="cardoneBooks galleryImg" src={cardoneBooks} alt="Grant Cardone" />
-                    <p className="caption">Grant Cardone's books</p>
+            <span>
 
-                    <img className="daringGreatly galleryImg" src={daringGreatly} alt="Daring Greatly" />
-                    <p className="caption">Daring Greatly by Brene Brown</p>
-                </div>
+            <div className="gallery">
                 <div>
-                    <img className="dietCults galleryImg" src={dietCults} alt="Diet Cults" />
-                    <p className="caption">Diet Cults</p>
+                    <img className="fiftyBooks galleryImg col-8" src={fiftyBooks} alt="Fifty Books" />
                 </div>
+
                 <div>
-                    <img className="fiftyBooks galleryImg" src={fiftyBooks} alt="Fifty Books" />
-                    <p className="caption">Over fifty books read in 2016</p>
+                    <img className="robertGreene galleryImg col-4 left" src={robertGreene} alt="Robert Greene" />
                 </div>
-            </Container>
+
+                <div>
+                    <img className="cardoneBooks galleryImg col-4 right" src={cardoneBooks} alt="Grant Cardone" />
+                </div>
+
+                <div>
+                    <img className="daringGreatly galleryImg col-4 left" src={daringGreatly} alt="Daring Greatly" />
+                </div>
+
+                <div>
+                    <img className="dietCults galleryImg col-4 right" src={dietCults} alt="Diet Cults" />
+                </div>
+
+                <div>
+                    <img className="earlyShelf galleryImg col-4 right" src={earlyShelf} alt="Early Shelf" />
+                </div>
+
+                <div>
+                    <img className="handstand galleryImg col-4 left" src={handstand} alt="Handstand" />
+                </div>
+
+                <div>
+                    <img className="firstFullShelf galleryImg col-4 right" src={firstFullShelf} alt="First Full Shelf" />
+                </div>
+
+                <div>
+                    <img className="midBooks galleryImg col-4 left" src={midBooks} alt="Some books" />
+                </div>
+
+                <div>
+                    <img className="allBooks galleryImg col-8" src={allBooks} alt="All Books" />
+                </div>
+            </div>
+
+            <Footer />
+            </span>
         )
     }
 }
