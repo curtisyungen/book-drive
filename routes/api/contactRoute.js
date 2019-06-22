efe185d948f4b2c31f05967b74eeed3e4da2e312
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-21 18:50:03 -0700

    Add files via upload

--- routes/api/contactRoute.js
+++ routes/api/contactRoute.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const BookController = require("../../controllers/BookController");
-const controller = new BookController();
+const ContactController = require("../../controllers/ContactController");
+const controller = new ContactController();
 
-router.get("/sendContactMessage", (req, res) => {
-    controller.sendContactMessage(req, res);
+router.get("/submitContactForm", (req, res) => {
+    controller.submitContactForm(req, res);
 });
 
 module.exports = router;
