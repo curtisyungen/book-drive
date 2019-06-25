71c079f042febbeb142d22b0b0bcc11289a59aa9
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-24 19:59:10 -0700

    Add files via upload

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -41,7 +41,7 @@ class Home extends Component {
         }
 
         this.setState({
-            // books: this.props.books,
+            books: this.props.books,
             message: message,
             userSearch: this.props.userSearch,
             sortOption: "",
@@ -53,7 +53,7 @@ class Home extends Component {
     componentDidUpdate = (prevProps) => {
         if (prevProps !== this.props) {
             this.setState({
-                // books: this.props.books,
+                books: this.props.books,
                 userSearch: this.props.userSearch,
             });
         }
