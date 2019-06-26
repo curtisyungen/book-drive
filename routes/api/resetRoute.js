d6a7b0a2638c89d880c8d39beef6633c288dcde3
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-25 20:02:27 -0700

    Add files via upload

--- routes/api/resetRoute.js
+++ routes/api/resetRoute.js
@@ -2,16 +2,16 @@ const router = require("express").Router();
 const ResetController = require("../../controllers/ResetController");
 const controller = new ResetController();
 
-router.get("/submitResetCode", (req, res) => {
-    controller.submitResetCode(req, res);
-});
-
 router.post("/setResetCode", (req, res) => {
     controller.setResetCode(req, res);
 });
 
 router.post("/sendPasswordResetCode", (req, res) => {
     controller.sendPasswordResetCode(req, res);
+});
+
+router.get("/submitResetCode", (req, res) => {
+    controller.submitResetCode(req, res);
 });
 
 router.put("/clearResetCode", (req, res) => {
