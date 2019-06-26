73ec2c71fb92b79ecf898978ca104859d2a394d9
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-26 15:31:49 -0700

    Add files via upload

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -11,7 +11,12 @@ class About extends Component {
                 <div className="about">
                     Congo is a project created to sell my extensive collection of self-help books.
 
-                    
+                    Over the years I've collected upwards of 470 books, most of them self-help books. 
+                    I haven't read many but not all of them. A good number have been partially read, and others haven't even been opened.
+
+                    Now the books serve only to impress visitors to my apartment as they sit piled on the floor collecting dust.
+
+                    I'm done helping myself; now I'd like to help you get your hands on some books...at a discount.
                 </div>
             </span>
         )
