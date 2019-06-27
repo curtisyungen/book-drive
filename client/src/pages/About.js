85f51928349ff55eef710ff5de8b7c87c6c946fe
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-27 11:01:12 -0700

    Add files via upload

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,48 +5,47 @@ class About extends Component {
     render() {
         return (
             <span>
-
                 <h4 className="aboutHeader">About</h4>
-
-                {/* ABOUT - INTRO */}
-
                 <div className="about">
-                    Congo is a project created to sell my extensive collection of self-help books.
 
-                    Over the years I've collected upwards of 470 books, most of them self-help books. 
-                    I haven't read many but not all of them. A good number have been partially read, and others haven't even been opened.
+                    {/* ABOUT - INTRO */}
+                    <div className="aboutIntro">
+                        Congo is a project created to sell my extensive collection of self-help books.
 
-                    Now the books serve only to impress visitors to my apartment as they sit piled on the floor collecting dust.
+                        Over the years I've collected upwards of 470 books, most of them self-help books.
+                        I haven't read many but not all of them. A good number have been partially read, and others haven't even been opened.
 
-                    I'm done helping myself; now I'd like to help you get your hands on some books...at a discount.
-                </div>
+                        Now the books serve only to impress visitors to my apartment as they sit piled on the floor collecting dust.
 
-                {/* ABOUT - BUSINESS */}
+                        I'm done helping myself; now I'd like to help you get your hands on some books...at a discount.
+                    </div>
 
-                <div className="aboutBusiness">
+                    {/* ABOUT - BUSINESS */}
+                    <div className="aboutBusiness">
 
-                </div>
+                    </div>
 
-                {/* ABOUT - TECHNICAL */}
+                    {/* ABOUT - TECHNICAL */}
+                    <div className="aboutTechnical">
+                        Everything on this site was created from scratch, including all graphics, logos, page headers, button icons, and scripts.
+                        Nothing but inspiration was taken from Amazon's website.
 
-                <div className="aboutTechnical">
-                    Everything on this site was created from scratch, including all graphics, logos, page headers, button icons, and scripts.
-                    Nothing but inspiration was taken from Amazon's website.
+                        The site was built using Node.js, Express.js, and ReactJS.
 
-                    The site is hosted on Heroku and built using Node.js, Express.js, and ReactJS. 
+                        <ul>Dependencies:
+                            <li>axios</li>
+                            <li>bcrypt</li>
+                            <li>express</li>
+                            <li>nodemailer</li>
+                            <li>paypal-rest-sdk</li>
+                            <li>react-dom</li>
+                            <li>react-responsive-modal</li>
+                            <li>react-router</li>
+                            <li>reactjs-popup</li>
+                            <li>sequelize</li>
+                        </ul>
+                    </div>
 
-                    Dependencies: 
-
-                    axios
-                    bcrypt
-                    express
-                    nodemailer
-                    paypal-rest-sdk
-                    react-dom
-                    react-responsive-modal
-                    react-router
-                    reactjs-popup
-                    sequelize
                 </div>
             </span>
         )
