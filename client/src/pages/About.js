40d06bdde62a59833b27e6f6c013d4f71e084a8c
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-27 20:36:11 -0700

    Add files via upload

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import stack2 from "../images/stacks2.jpg";
 import "./About.css";
 
 class About extends Component {
@@ -10,9 +11,9 @@ class About extends Component {
 
                     {/* ABOUT - INTRO */}
                     <div className="aboutSection aboutIntro">
-                        Congo is a project created to sell my extensive collection of self-help books.
+                        Congo is a project created to sell my extensive collection of books.
 
-                        Over the years I've collected upwards of 470 books, most of them self-help books.
+                        Over the years I've collected upwards of 460 books, most of them self-help books.
                         I haven't read many but not all of them. A good number have been partially read, and others haven't even been opened.
 
                         Now the books serve only to impress visitors to my apartment as they sit piled on the floor collecting dust.
@@ -20,43 +21,65 @@ class About extends Component {
                         I'm done helping myself; now I'd like to help you get your hands on some books...at a discount.
                     </div>
 
-                    {/* ABOUT - BUSINESS */}
-                    <div className="aboutSection aboutBusiness">
+                    <div className="aboutSection aboutTechnical">
+                        Everything on this site was created from scratch including all graphics, logos, page headers, button icons, and scripts.
+                        The only things taken from Amazon were the book cover images (as cited) and genuine inspiration from their beautiful and high-quality website.
+                    </div>
 
+                    {/* ABOUT - BOOKS */}
+                    <h4 className="aboutTitle">The Books</h4>
+                    <div className="aboutSection aboutBusiness">
+                        All books are used, but their conditions vary widely. Some books may be brand new and unblemished; others may have dog-eared pages, highlights, or tear stains.
+                        Every book is listed at a discounted rate than the normal market rate. Shipping is free. The actual book covers may vary from the
+                        cover shown on the website.
+
+                        Books will be up for sale until August 1, 2019. After that, they will be donated to a local charity.
                     </div>
 
                     {/* ABOUT - TECHNICAL */}
-                    <div className="aboutSection aboutTechnical">
-                        Everything on this site was created from scratch, including all graphics, logos, page headers, button icons, and scripts.
-                        The only things taken from Amazon were the book cover images (as cited) and genuine inspiration from their beautiful and high-quality website.
+                    <h4 className="aboutTitle">The Technology</h4>
+                    <div className="aboutSection">
+                        Payments are handled by PayPal rather than directly by Congo.
+                        User data is private and all passwords are encrypted.
+                    </div>
 
-                        {/* TECHNOLOGY LIST */}
-                        <div className="aboutSection">
-                            Technologies Used
-                            <ul className="technologyList">
-                                <li className="technology">MySQL</li>
-                                <li className="technology">Express.js</li>
-                                <li className="technology">ReactJS</li>
-                                <li className="technology">Node.js</li>
-                            </ul>
-                        </div>
+                    <div className="aboutSection">
+                        Technologies Used
+                        <ul className="technologyList">
+                            <li className="technology">MySQL</li>
+                            <li className="technology">Express.js</li>
+                            <li className="technology">ReactJS</li>
+                            <li className="technology">Node.js</li>
+                        </ul>
+                    </div>
 
-                        {/* DEPENDENCY LIST */}
-                        <div className="aboutSection">
-                            Dependencies
-                            <ul className="dependencyList">
-                                <li className="dependency">axios</li>
-                                <li className="dependency">bcrypt</li>
-                                <li className="dependency">express</li>
-                                <li className="dependency">nodemailer</li>
-                                <li className="dependency">paypal-rest-sdk</li>
-                                <li className="dependency">react-dom</li>
-                                <li className="dependency">react-responsive-modal</li>
-                                <li className="dependency">react-router</li>
-                                <li className="dependency">reactjs-popup</li>
-                                <li className="dependency">sequelize</li>
-                            </ul>
-                        </div>
+                    <div className="aboutSection">
+                        Dependencies
+                        <ul className="dependencyList">
+                            <li className="dependency">axios</li>
+                            <li className="dependency">bcrypt</li>
+                            <li className="dependency">express</li>
+                            <li className="dependency">nodemailer</li>
+                            <li className="dependency">paypal-rest-sdk</li>
+                            <li className="dependency">react-dom</li>
+                            <li className="dependency">react-responsive-modal</li>
+                            <li className="dependency">react-router</li>
+                            <li className="dependency">reactjs-popup</li>
+                            <li className="dependency">sequelize</li>
+                        </ul>
+                    </div>
+
+                    {/* ABOUT - CREATOR */}
+                    <h4 className="aboutTitle">The Person</h4>
+                    <div className="aboutSection aboutCreator">
+                        This website was designed and built by Curtis Yungen. Curtis is a Boeing engineer and web developer, designing planes by day and websites by night. 
+                        Check out his <a href="https://curtisyungen.github.io/Portfolio" target="_blank" rel="noopener noreferrer">online portfolio</a>.
+                    </div>
+
+                    <h4 className="aboutTitle">The Inspiration</h4>
+                    <div className="aboutSection">
+                        This website was born from a love of two things: reading and coding, as well as a deep admiration for Amazon and its amazing website. 
+                        The highest form of flattery is imitation. 
                     </div>
                 </div>
             </span>
