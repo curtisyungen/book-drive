9bb4a562caac9eb0e58f9ff02f1c6f80ee475c15
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-06-29 16:27:18 -0700

    Add files via upload

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -3,7 +3,17 @@ import Footer from "../components/Footer/footer";
 import "./Error.css";
 
 class Error extends Component {
-
+    render() {
+        return (
+            <span>
+                <div className="errorPage">
+                    <p>Page not found.</p>
+                </div>
+                
+                <Footer />
+            </span>
+        )
+    }
 }
 
 export default Error;
