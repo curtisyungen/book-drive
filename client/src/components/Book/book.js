1bcbbe70f659ab28e4a62e23d7b689fa7ce892ee
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-01 09:55:09 -0700

    Add files via upload

--- client/src/components/Book/book.js
+++ client/src/components/Book/book.js
@@ -54,7 +54,7 @@ class Book extends Component {
         API.getBookByTitle(this.state.title)
             .then((res) => {
                 this.setState({
-                    description: res.data.items[0].volumeInfo.description.replace('\ufffc', ''),
+                    description: res.data.items[0].volumeInfo.description.replace('\ufffd', ''),
                     loadingDetailView: false,
                 });
             });
